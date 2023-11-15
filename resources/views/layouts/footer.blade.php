8d69fe6fd88df2b360975914b247fbcacf0a76bd
marielena08 <marielenan_20@alu.uabcs.mx>
2023-11-14 21:20:56 -0700

    Carpeta layouts maquetacion

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="content-footer footer bg-footer-theme d-flex align-items-center justify-content-center">
+    <div class="container-xxl">
+        <div class="footer-container text-center">
+            <div class="d-none d-lg-inline-block">
+            <h>UNIVERSIDAD AUTONOMA DE BAJA CALIFORNIA SUR</h>
+                <p>Equipo 2 - Sistema de Gimnasio - GymXpert:</p>
+            </div>
+        </div>
+    </div>
+</footer>
