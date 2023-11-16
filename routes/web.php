d95a0020b5e493375f5c8270e6b37c939b97920b
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-11-16 14:49:01 -0700

    Routas store, update y destroy de customers añadidas.

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,21 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+Route::get('/test', function () {
+    return view('test');
+});
+
+Route::controller(CustomerController::class)->prefix('/customers')->group(function () {
+    Route::get('/', 'index')->name('customers');
+    Route::post('/', 'store')->name('customers.store');
+    Route::get('/{id}','show')->name('customers.show');
+    Route::put('/{id}','update')->name('customers.update');
+    Route::delete('/{id}','destroy')->name('customers.delete');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/', function () {
         return view('home');
-    });
-
-    Route::controller(CustomerController::class)->prefix('/customers')->group(function () {
-        Route::get('/', 'index')->name('customers.index');
-        Route::get('/{id}','show')->name('customers.show');
     });
 
     Route::get('/fares', function () {
