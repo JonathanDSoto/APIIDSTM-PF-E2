2ead580d7f32d77069218b852dc0dd390c254546
marielena08 <marielenan_20@alu.uabcs.mx>
2023-11-15 22:27:19 -0700

    Maquetacion Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,9 +14,6 @@
           <h4 class="mb-0">75 clientes</h4>
           <small class="text-danger">75%</small>
         </div>
-        <div class="pt-1">
-          <span class="badge bg-label-secondary">+8.2%</span>
-        </div>
       </div>
     </div>
   </div>
@@ -32,9 +29,6 @@
           <h4 class="mb-0">15 clientes</h4>
           <small class="text-success">15%</small>
         </div>
-        <div class="pt-1">
-          <span class="badge bg-label-secondary">-5.2%</span>
-        </div>
       </div>
     </div>
   </div>
@@ -47,9 +41,6 @@
         <h5 class="card-title mb-1 pt-2">Ingresos Mensuales</h5>
         <small class="text-muted">Mensual</small>
         <p class="mb-2 mt-1">$1.28k</p>
-        <div class="pt-1">
-          <span class="badge bg-label-secondary">+12.2%</span>
-        </div>
       </div>
     </div>
   </div>
@@ -62,33 +53,10 @@
         <h5 class="card-title mb-1 pt-2">Ingresos Anuales</h5>
         <small class="text-muted">Anual</small>
         <p class="mb-2 mt-1">$4,673</p>
-        <div class="pt-1">
-          <span class="badge bg-label-secondary">+25.2%</span>
-        </div>
       </div>
     </div>
   </div>
 
-  <!-- Revenue Growth -->
-  <!-- <div class="col-xl-4 col-md-8 mb-4">
-    <div class="card">
-      <div class="card-body">
-        <div class="d-flex justify-content-between">
-          <div class="d-flex flex-column">
-            <div class="card-title mb-auto">
-              <h5 class="mb-1 text-nowrap">Revenue Growth</h5>
-              <small>Weekly Report</small>
-            </div>
-            <div class="chart-statistics">
-              <h3 class="card-title mb-1">$4,673</h3>
-              <span class="badge bg-label-success">+15.2%</span>
-            </div>
-          </div>
-          <div id="revenueGrowth"></div>
-        </div>
-      </div>
-    </div>
-  </div> -->
   <!-- INSTRUCTORES -->
   <div class="col-xl-6 col-md-6 mb-4">
     <div class="card h-100">
@@ -106,89 +74,75 @@
       <div class="card-body">
         <ul class="p-0 m-0">
           <li class="d-flex mb-4 pb-1 align-items-center">
-            <img src="../../assets/img/icons/brands/chrome.png" alt="Chrome" height="28" class="me-3 rounded">
+            <img src="../../assets/img/avatars/5.png" height="28" class="me-3 rounded">
             <div class="d-flex w-100 align-items-center gap-2">
               <div class="d-flex justify-content-between flex-grow-1 flex-wrap">
                 <div>
-                  <h6 class="mb-0">Google Chrome</h6>
+                  <h6 class="mb-0">James Smith</h6>
                 </div>
 
                 <div class="user-progress d-flex align-items-center gap-2">
-                  <h6 class="mb-0">90.4%</h6>
+                  <h6 class="mb-0">80.4%</h6>
                 </div>
               </div>
               <div class="chart-progress" data-color="secondary" data-series="85"></div>
             </div>
           </li>
           <li class="d-flex mb-4 pb-1 align-items-center">
-            <img src="../../assets/img/icons/brands/safari.png" alt="Safari" height="28" class="me-3 rounded">
+            <img src="../../assets/img/avatars/8.png"  height="28" class="me-3 rounded">
             <div class="d-flex w-100 align-items-center gap-2">
               <div class="d-flex justify-content-between flex-grow-1 flex-wrap">
                 <div>
-                  <h6 class="mb-0">Apple Safari</h6>
+                  <h6 class="mb-0">Robert Williams</h6>
                 </div>
                 <div class="user-progress d-flex align-items-center gap-2">
-                  <h6 class="mb-0">70.6%</h6>
+                  <h6 class="mb-0">10.6%</h6>
                 </div>
               </div>
               <div class="chart-progress" data-color="success" data-series="70"></div>
             </div>
           </li>
           <li class="d-flex mb-4 pb-1 align-items-center">
-            <img src="../../assets/img/icons/brands/firefox.png" alt="Firefox" height="28" class="me-3 rounded">
+            <img src="../../assets/img/avatars/7.png" height="28" class="me-3 rounded">
             <div class="d-flex w-100 align-items-center gap-2">
               <div class="d-flex justify-content-between flex-grow-1 flex-wrap">
                 <div>
-                  <h6 class="mb-0">Mozilla Firefox</h6>
+                  <h6 class="mb-0">Michael Jones</h6>
                 </div>
                 <div class="user-progress d-flex align-items-center gap-2">
-                  <h6 class="mb-0">35.5%</h6>
+                  <h6 class="mb-0">5.5%</h6>
                 </div>
               </div>
               <div class="chart-progress" data-color="primary" data-series="25"></div>
             </div>
           </li>
           <li class="d-flex mb-4 pb-1 align-items-center">
-            <img src="../../assets/img/icons/brands/opera.png" alt="Opera" height="28" class="me-3 rounded">
+            <img src="../../assets/img/avatars/6.png"  height="28" class="me-3 rounded">
             <div class="d-flex w-100 align-items-center gap-2">
               <div class="d-flex justify-content-between flex-grow-1 flex-wrap">
                 <div>
-                  <h6 class="mb-0">Opera Mini</h6>
+                  <h6 class="mb-0">Richard Miller</h6>
                 </div>
 
                 <div class="user-progress d-flex align-items-center gap-2">
-                  <h6 class="mb-0">80.0%</h6>
+                  <h6 class="mb-0">2.0%</h6>
                 </div>
               </div>
               <div class="chart-progress" data-color="danger" data-series="75"></div>
             </div>
           </li>
           <li class="d-flex mb-4 pb-1 align-items-center">
-            <img src="../../assets/img/icons/brands/edge.png" alt="Edge" height="28" class="me-3 rounded">
+            <img src="../../assets/img/avatars/2.png" height="28" class="me-3 rounded">
             <div class="d-flex w-100 align-items-center gap-2">
               <div class="d-flex justify-content-between flex-grow-1 flex-wrap">
                 <div>
-                  <h6 class="mb-0">Internet Explorer</h6>
+                  <h6 class="mb-0">Christopher Anderson</h6>
                 </div>
                 <div class="user-progress d-flex align-items-center gap-2">
-                  <h6 class="mb-0">62.2%</h6>
+                  <h6 class="mb-0">1.5%</h6>
                 </div>
               </div>
               <div class="chart-progress" data-color="info" data-series="60"></div>
-            </div>
-          </li>
-          <li class="d-flex align-items-center">
-            <img src="../../assets/img/icons/brands/brave.png" alt="Brave" height="28" class="me-3 rounded">
-            <div class="d-flex w-100 align-items-center gap-2">
-              <div class="d-flex justify-content-between flex-grow-1 flex-wrap">
-                <div>
-                  <h6 class="mb-0">Brave</h6>
-                </div>
-                <div class="user-progress d-flex align-items-center gap-2">
-                  <h6 class="mb-0">46.3%</h6>
-                </div>
-              </div>
-              <div class="chart-progress" data-color="warning" data-series="45"></div>
             </div>
           </li>
         </ul>
@@ -212,89 +166,75 @@
       <div class="card-body">
         <ul class="p-0 m-0">
           <li class="d-flex mb-4 pb-1 align-items-center">
-            <img src="../../assets/img/icons/brands/chrome.png" alt="Chrome" height="28" class="me-3 rounded">
+            <img src="../../assets/img/favicon/ejercicio.png" alt="Chrome" height="28" class="me-3 rounded">
             <div class="d-flex w-100 align-items-center gap-2">
               <div class="d-flex justify-content-between flex-grow-1 flex-wrap">
                 <div>
-                  <h6 class="mb-0">Google Chrome</h6>
+                  <h6 class="mb-0">Entrenamiento Funcional</h6>
                 </div>
 
                 <div class="user-progress d-flex align-items-center gap-2">
-                  <h6 class="mb-0">90.4%</h6>
+                  <h6 class="mb-0">70.4%</h6>
                 </div>
               </div>
               <div class="chart-progress" data-color="secondary" data-series="85"></div>
             </div>
           </li>
           <li class="d-flex mb-4 pb-1 align-items-center">
-            <img src="../../assets/img/icons/brands/safari.png" alt="Safari" height="28" class="me-3 rounded">
+          <img src="../../assets/img/favicon/ejercicio.png" alt="Chrome" height="28" class="me-3 rounded">
             <div class="d-flex w-100 align-items-center gap-2">
               <div class="d-flex justify-content-between flex-grow-1 flex-wrap">
                 <div>
-                  <h6 class="mb-0">Apple Safari</h6>
+                  <h6 class="mb-0">Spinning</h6>
                 </div>
                 <div class="user-progress d-flex align-items-center gap-2">
-                  <h6 class="mb-0">70.6%</h6>
+                  <h6 class="mb-0">20.6%</h6>
                 </div>
               </div>
               <div class="chart-progress" data-color="success" data-series="70"></div>
             </div>
           </li>
           <li class="d-flex mb-4 pb-1 align-items-center">
-            <img src="../../assets/img/icons/brands/firefox.png" alt="Firefox" height="28" class="me-3 rounded">
+          <img src="../../assets/img/favicon/ejercicio.png" alt="Chrome" height="28" class="me-3 rounded">
             <div class="d-flex w-100 align-items-center gap-2">
               <div class="d-flex justify-content-between flex-grow-1 flex-wrap">
                 <div>
-                  <h6 class="mb-0">Mozilla Firefox</h6>
+                  <h6 class="mb-0">Yoga</h6>
                 </div>
                 <div class="user-progress d-flex align-items-center gap-2">
-                  <h6 class="mb-0">35.5%</h6>
+                  <h6 class="mb-0">5.3%</h6>
                 </div>
               </div>
               <div class="chart-progress" data-color="primary" data-series="25"></div>
             </div>
           </li>
           <li class="d-flex mb-4 pb-1 align-items-center">
-            <img src="../../assets/img/icons/brands/opera.png" alt="Opera" height="28" class="me-3 rounded">
+          <img src="../../assets/img/favicon/ejercicio.png" alt="Chrome" height="28" class="me-3 rounded">
             <div class="d-flex w-100 align-items-center gap-2">
               <div class="d-flex justify-content-between flex-grow-1 flex-wrap">
                 <div>
-                  <h6 class="mb-0">Opera Mini</h6>
+                  <h6 class="mb-0">Pilates</h6>
                 </div>
 
                 <div class="user-progress d-flex align-items-center gap-2">
-                  <h6 class="mb-0">80.0%</h6>
+                  <h6 class="mb-0">3.1%</h6>
                 </div>
               </div>
               <div class="chart-progress" data-color="danger" data-series="75"></div>
             </div>
           </li>
           <li class="d-flex mb-4 pb-1 align-items-center">
-            <img src="../../assets/img/icons/brands/edge.png" alt="Edge" height="28" class="me-3 rounded">
+          <img src="../../assets/img/favicon/ejercicio.png" alt="Chrome" height="28" class="me-3 rounded">
             <div class="d-flex w-100 align-items-center gap-2">
               <div class="d-flex justify-content-between flex-grow-1 flex-wrap">
                 <div>
-                  <h6 class="mb-0">Internet Explorer</h6>
+                  <h6 class="mb-0">Boxeo</h6>
                 </div>
                 <div class="user-progress d-flex align-items-center gap-2">
-                  <h6 class="mb-0">62.2%</h6>
+                  <h6 class="mb-0">1.2%</h6>
                 </div>
               </div>
               <div class="chart-progress" data-color="info" data-series="60"></div>
-            </div>
-          </li>
-          <li class="d-flex align-items-center">
-            <img src="../../assets/img/icons/brands/brave.png" alt="Brave" height="28" class="me-3 rounded">
-            <div class="d-flex w-100 align-items-center gap-2">
-              <div class="d-flex justify-content-between flex-grow-1 flex-wrap">
-                <div>
-                  <h6 class="mb-0">Brave</h6>
-                </div>
-                <div class="user-progress d-flex align-items-center gap-2">
-                  <h6 class="mb-0">46.3%</h6>
-                </div>
-              </div>
-              <div class="chart-progress" data-color="warning" data-series="45"></div>
             </div>
           </li>
         </ul>
@@ -327,32 +267,10 @@
               <td>
                 <div class="d-flex justify-content-start align-items-center">
                   <div class="me-3">
-                    <!-- <img src="../../assets/img/icons/payments/visa-img.png" alt="Visa" height="30"> -->
+                    <img src="../../assets/img/illustrations/page-pricing-basic.png" alt="Visa" height="45">
                   </div>
                   <div class="d-flex flex-column">
-                    <p class="mb-0 fw-medium">Dia</p><small class="text-muted">Prueba</small>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <div class="d-flex flex-column">
-                  <p class="mb-0 fw-medium">8</p>
-                  <small class="text-muted text-nowrap">clientes</small>
-                </div>
-              </td>
-              <td><span class="badge bg-label-success">$0.00</span></td>
-              <td>
-                <p class="mb-0 fw-medium">+$0.00</p>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div class="d-flex justify-content-start align-items-center">
-                  <div class="me-3">
-                    <!-- <img src="../../assets/img/icons/payments/visa-img.png" alt="Visa" height="30"> -->
-                  </div>
-                  <div class="d-flex flex-column">
-                    <p class="mb-0 fw-medium">Dia</p><small class="text-muted">normal</small>
+                    <p class="mb-0 fw-medium">Dia</p>
                   </div>
                 </div>
               </td>
@@ -371,44 +289,44 @@
               <td>
                 <div class="d-flex justify-content-start align-items-center">
                   <div class="me-3">
-                    <!-- <img src="../../assets/img/icons/payments/visa-img.png" alt="Visa" height="30"> -->
+                    <img src="../../assets/img/illustrations/page-pricing-basic.png" alt="Visa" height="45">
                   </div>
                   <div class="d-flex flex-column">
-                    <p class="mb-0 fw-medium">Mensual</p><small class="text-muted">xxxxxx</small>
+                    <p class="mb-0 fw-medium">Mensual</p>
                   </div>
                 </div>
               </td>
               <td>
                 <div class="d-flex flex-column">
-                  <p class="mb-0 fw-medium">50</p>
+                  <p class="mb-0 fw-medium">70</p>
                   <small class="text-muted text-nowrap">clientes</small>
                 </div>
               </td>
-              <td><span class="badge bg-label-success">$0.00</span></td>
+              <td><span class="badge bg-label-success">$450.00</span></td>
               <td>
-                <p class="mb-0 fw-medium">+$0.00</p>
+                <p class="mb-0 fw-medium">+$31,500.00</p>
               </td>
             </tr>
             <tr>
               <td>
                 <div class="d-flex justify-content-start align-items-center">
                   <div class="me-3">
-                    <!-- <img src="../../assets/img/icons/payments/visa-img.png" alt="Visa" height="30"> -->
+                    <img src="../../assets/img/illustrations/page-pricing-basic.png" alt="Visa" height="45">
                   </div>
                   <div class="d-flex flex-column">
-                    <p class="mb-0 fw-medium">Dia</p><small class="text-muted">Prueba</small>
+                    <p class="mb-0 fw-medium">Anual</p>
                   </div>
                 </div>
               </td>
               <td>
                 <div class="d-flex flex-column">
-                  <p class="mb-0 fw-medium">20</p>
+                  <p class="mb-0 fw-medium">18</p>
                   <small class="text-muted text-nowrap">clientes</small>
                 </div>
               </td>
-              <td><span class="badge bg-label-success">$0.00</span></td>
+              <td><span class="badge bg-label-success">$3000.00</span></td>
               <td>
-                <p class="mb-0 fw-medium">+$0.00</p>
+                <p class="mb-0 fw-medium">+$54,000.00</p>
               </td>
             </tr>
 
