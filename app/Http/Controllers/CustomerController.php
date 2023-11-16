09bf14486225e104302b0d7f07193a22c585535d
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-11-15 23:38:14 -0700

    CustomerController creado.

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function index()
+    {
+        return view('customers.index', ['customersPagination' => Customer::simplePaginate(15)]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(string $id)
+    {
+        $customer = Customer::find($id);
+        $customer->bloodGroup;
+
+        return view('customers.show', ['customer' => $customer]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
