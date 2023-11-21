2633e23fd20ef48e100613f428efa847f47b28e1
adalbertog20 <adalbertog_20@alu.uabcs.mx>
2023-11-21 12:25:57 -0700

    Fix Routes not Found

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,25 @@
-import './bootstrap';
-import { createApp } from 'vue';
+import "./bootstrap";
+import { createApp } from "vue";
+import route from "../../vendor/tightenco/ziggy";
 
-const app = createApp({})
+const app = createApp({});
 
-Object.entries(import.meta.glob('./**/*.vue', { eager: true }))
-    .forEach(([path, definition]) => {
-        app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
-    });
+Object.entries(import.meta.glob("./**/*.vue", { eager: true })).forEach(
+    ([path, definition]) => {
+        app.component(
+            path
+                .split("/")
+                .pop()
+                .replace(/\.\w+$/, ""),
+            definition.default,
+        );
+    },
+);
 
 app.mixin({
     methods: {
-        route
-    }
-})
+        route,
+    },
+});
 
-app.mount('#app');
+app.mount("#app");
