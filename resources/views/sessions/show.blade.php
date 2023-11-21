183ae95e71819f5372efc6babbec7c486ba19b9e
marielena08 <marielenan_20@alu.uabcs.mx>
2023-11-21 11:48:36 -0700

    maquetacion modal editar clase

--- resources/views/sessions/show.blade.php
+++ resources/views/sessions/show.blade.php
@@ -14,7 +14,7 @@
                             <p>"Haz que cada movimiento cuente, entrena para la vida diaria."</p>
                         </div>
                         <div class="order-md-2">
-                            <a class="btn btn-primary" href="javascript:void(0);">
+                            <a class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editSession" href="javascript:void(0);">
                                 <i class="ti ti-pencil me-1"></i> Editar clase 
                             </a>
                         </div>
@@ -262,5 +262,69 @@
         </div>
         <!--/ CLIENTES POR CLASE -->
     </div>
+
+    <!-- EDITAR CLASE -->
+    <div class="modal fade" id="editSession" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-simple modal-enable-otp modal-dialog-centered">
+            <div class="modal-content p-3 p-md-5">
+                <div class="modal-body">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <div class="text-center">
+                        <h3 class="mb-2">Edita la clase</h3>
+                        <p>Edita los datos permitidos</p>
+                    </div>
+                    <form id="EditCustomerSession" class="row g-3" onsubmit="return false">
+                        <div class="col-12 col-md-12">
+                            <label for="selectCustomer" class="form-label">Ingresa el nombre de la clase</label>
+                            <input type="text" id="modalEditSession" name="modalEditSession" class="form-control" placeholder="Entrenamiento Funcional" />
+                        </div>
+                        <div class="row">
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="horaInicial" class="form-label">Hora Inicial:</label>
+                                    <input type="time" class="form-control" id="horaInicial" name="horaInicialSession">
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="horaFinal" class="form-label">Hora Final:</label>
+                                    <input type="time" class="form-control" id="horaFinal" name="horaFinalSession">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <label class="form-label" for="modalSessionInstructor">Entrenador/Instructor</label>
+                            <input type="text" id="modalSessionInstructor" name="modalSessionInstructor" class="form-control" placeholder="Mark Martin" />
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <label for="selectDay" class="form-label">Selecciona el dia</label>
+                                <select id="selectDay" class="form-select form-select-lg share-project-select" data-allow-clear="true">
+                                <option value="dia" select>Dia de la semana</option>
+                                <option value="lunes">Lunes</option>
+                                <option value="martes">Martes</option>
+                                <option value="miercoles">Miercoles </option>
+                                <option value="jueves">Jueves</option>
+                                <option value="viernes">Viernes </option>
+                                <option value="sabado">Sabado </option>
+                            </select>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <label for="descripcion" class="form-label">Descripción:</label>
+                            <textarea class="form-control" id="descripcion" name="descripcion" rows="2"></textarea>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <label for="capacity" class="form-label">Capacidad Maxima de personas:</label>
+                            <input type="number" class="form-control" id="numero" name="numero" >
+                        </div>
+                        <div class="col-12 text-center">
+                            <button type="submit" class="btn btn-primary me-sm-3 me-1">Editar</button>
+                            <button type="reset" class="btn btn-label-secondary" data-bs-dismiss="modal" aria-label="Close">Cancelar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ EDITAR CLASE -->
 </div> 
 @endsection
