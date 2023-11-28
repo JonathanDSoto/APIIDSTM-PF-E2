11e40010ccbc75a5175cd9bf75c8b3b887e190ef
IngenieroAlan <brandon.rod182@gmail.com>
2023-11-28 16:48:32 -0700

    Navbar component updated
    - logout fixed

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,4 +1,6 @@
 <script setup>
+import FormAuth from './FormAuth.vue';
+
 const props = defineProps({
     user: {
         type: Object,
@@ -84,20 +86,17 @@ console.log(props.user);
                         <li>
                             <form
                                 id="logout-form"
-                                action="{{ route('logout') }}"
+                                :action="route('logout')"
                                 method="POST"
-                                style="display: none"
                             >
-                                @csrf
-                            </form>
-                            <a
+                            <FormAuth method="POST"/>
+                                <button
                                 class="dropdown-item"
-                                href="{{ route('logout') }}"
-                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                            >
+                                >
                                 <i class="ti ti-logout me-2 ti-sm"></i>
                                 <span class="align-middle">Cerrar Sesion</span>
-                            </a>
+                            </button>
+                        </form>
                         </li>
                     </ul>
                 </li>
