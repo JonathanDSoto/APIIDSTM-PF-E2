e89a56141944945f51fba798a6126415e9dc2dd0
IngenieroAlan <brandon.rod182@gmail.com>
2023-11-30 16:39:07 -0700

    DeleteModal component updated
    - Design updated

--- resources/js/components/DeleteModal.vue
+++ resources/js/components/DeleteModal.vue
@@ -11,57 +11,47 @@ const props = defineProps({
         required: true,
     },
 });
+
+const getTranslation = (type) => {
+    switch (type) {
+        case 'customers':
+            return 'al cliente';
+        case 'instructors':
+            return 'al instructor';
+        case 'fares':
+            return 'la tarifa';
+        default:
+            return '';
+    }
+}
+
 </script>
 
 <template>
-    <form
-        method="POST"
-        id="deleteUserForm"
-        :action="route(`${props.type}.delete`, { id: props.id })"
-    >
+    <form method="POST" id="deleteUserForm" :action="route(`${props.type}.delete`, { id: props.id })"
+        class="d-flex justify-content-center align-items-center vh-100">
         <FormAuth method="delete" />
-        <div
-            class="modal fade"
-            id="deleteUser"
-            tabindex="-1"
-            aria-hidden="true"
-        >
-            <div class="modal-dialog modal-lg modal-simple modal-edit-user">
+        <div class="modal fade" id="deleteUser" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-lg modal-simple modal-edit-user">
                 <div class="modal-content p-3 p-md-5">
                     <div class="modal-body">
-                        <button
-                            type="button"
-                            class="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                        ></button>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         <div class="text-center mb-4">
                             <h3 class="mb-2">
                                 Eliminar
-                                {{ props.type }}
+                                {{ getTranslation(props.type) }}
                             </h3>
                             <p class="text-muted">
-                                ¿Estas Seguro que deseas eliminar
-                                {{ props.type }}
+                                ¿Estás seguro que deseas eliminar
+                                {{ getTranslation(props.type) }}?
                             </p>
                         </div>
-                        <!-- <form id="deleteUserForm" class="row g-3" :action="route('customers.delete', { id: props.id })
-                            " method="POST">
-                            <FormAuth method="POST" /> -->
                         <div class="col-12 text-center">
-                            <button
-                                type="submit"
-                                class="btn btn-primary me-sm-3 me-1"
-                            >
-                                Confirmo
+                            <button type="submit" class="btn btn-danger me-sm-3 me-1">
+                                Eliminar
                             </button>
-                            <button
-                                type="reset"
-                                class="btn btn-label-secondary"
-                                data-bs-dismiss="modal"
-                                aria-label="Close"
-                            >
-                                Cancel
+                            <button type="reset" class="btn btn-label-secondary" data-bs-dismiss="modal" aria-label="Close">
+                                Cancelar
                             </button>
                         </div>
                     </div>
