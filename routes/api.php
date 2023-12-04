6532b806f6206ccd330f9d5f2472c6925d38811f
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-12-03 19:49:40 -0700

    Rutas para API creadas.

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,32 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\BloodGroupController;
+use App\Http\Controllers\Api\CustomerController;
+use App\Http\Controllers\Api\FareController;
+use App\Http\Controllers\Api\FarePeriodController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PaymentController;
+use App\Http\Controllers\Api\PaymentTypeController;
+use App\Http\Controllers\Api\SessionController;
+use App\Http\Controllers\Api\SessionDayController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::prefix('v1')->group(function () {
+    Route::post('sync-account', [AuthController::class, 'syncAccount']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('sign-up', [AuthController::class, 'SignUp']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::post('session-days/subscribe', [SessionDayController::class, 'subscribe']);
+    Route::delete('session-days/cancel-subscription', [SessionDayController::class, 'cancelSubscription']);
+
+    Route::post('fares/pay', [FareController::class, 'pay']);
+
+    Route::apiResource('customers', CustomerController::class)->only(['show', 'update']);
+    Route::apiResource('sessions', SessionController::class)->only(['index', 'show']);
+    Route::apiResource('fares', FareController::class)->only(['index', 'show']);
+    Route::apiResource('blood-groups', BloodGroupController::class)->only(['index', 'show']);
+    Route::apiResource('payments', PaymentController::class)->only(['index', 'show']);
+    Route::apiResource('payment-types', PaymentTypeController::class)->only(['index', 'show']);
+    Route::apiResource('fare-periods', FarePeriodController::class)->only(['index', 'show']);
+    Route::apiResource('session-days', SessionDayController::class)->only(['index', 'show']);
 });
