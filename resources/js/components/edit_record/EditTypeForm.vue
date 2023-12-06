455c7d1ace4257047cc741cf74e782cd257b4509
adalbertog20 <adalbertog_20@alu.uabcs.mx>
2023-12-06 14:01:28 -0700

    Edit PaymentTypes Working

---
+++ resources/js/components/edit_record/EditTypeForm.vue
@@ -0,0 +1,70 @@
+<script setup>
+const props = defineProps({
+    tipo: {
+        type: Object,
+    },
+});
+</script>
+<template>
+    <div
+        class="modal fade"
+        id="editPaymentType"
+        tabindex="-1"
+        aria-hidden="true"
+    >
+        <div class="modal-dialog modal-lg modal-simple modal-new-customer">
+            <div class="modal-content p-3 p-md-5">
+                <div class="modal-body">
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                    ></button>
+                    <div class="text-center mb-4">
+                        <h3 class="mb-2">Editar Pago de Cliente</h3>
+                        <p class="text-muted">Completa los datos.</p>
+                    </div>
+                    <form
+                        :action="route('payment_types.update', { id: tipo.id })"
+                        method="post"
+                        id="paymentCustomer"
+                        class="row g-3"
+                    >
+                        <FormAuth method="PUT" />
+                        <div class="col-12">
+                            <label
+                                class="form-label"
+                                for="modalEditCustomerName"
+                                >Nombre completo</label
+                            >
+                            <input
+                                type="text"
+                                id="modalEditCustomerName"
+                                name="name"
+                                class="form-control"
+                                v-model="tipo.name"
+                            />
+                        </div>
+                        <div class="col-12 text-center">
+                            <button
+                                type="submit"
+                                class="btn btn-primary me-sm-3 me-1"
+                            >
+                                Pagar
+                            </button>
+                            <button
+                                type="reset"
+                                class="btn btn-label-secondary"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            >
+                                Cancelar
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- resources/js/components/payment_types/TableTypes.vue
+++ resources/js/components/payment_types/TableTypes.vue
@@ -1,5 +1,6 @@
 <script setup>
 import DeleteModal from "../DeleteModal.vue";
+import EditTypeForm from "../edit_record/EditTypeForm.vue";
 import { ref } from "vue";
 const props = defineProps({
     types: {
@@ -8,6 +9,8 @@ const props = defineProps({
 });
 
 const selectedTypeId = ref(0);
+const selectedIndex = ref(0);
+console.log(props.types);
 </script>
 <template>
     <div class="table-responsive text-nowrap">
@@ -19,12 +22,15 @@ const selectedTypeId = ref(0);
                 </tr>
             </thead>
             <tbody class="table-border-bottom-0">
-                <tr v-for="type in props.types" :key="type.id">
+                <tr v-for="(type, index) in props.types" :key="type.id">
                     <td>{{ type.name }}</td>
                     <td class="text-end">
                         <a
+                            @click="selectedIndex = index"
                             class="btn btn-success me-1"
                             href="javascript:void(0);"
+                            data-bs-toggle="modal"
+                            data-bs-target="#editPaymentType"
                         >
                             <i class="ti ti-pencil"></i>
                         </a>
@@ -42,5 +48,6 @@ const selectedTypeId = ref(0);
             </tbody>
         </table>
     </div>
+    <EditTypeForm :tipo="props.types[selectedIndex]" />
     <DeleteModal function="delete" type="payment_types" :id="selectedTypeId" />
 </template>
