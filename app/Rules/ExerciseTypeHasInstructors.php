9e7b8c77bd440ecfdb5715b96ebc51a6990b37dc
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-12-05 18:50:39 -0700

    Reglas para tipos de ejercicios corregidas.

--- app/Rules/ExerciseTypeHasInstructors.php
+++ app/Rules/ExerciseTypeHasInstructors.php
@@ -3,6 +3,7 @@
 namespace App\Rules;
 
 use App\Models\ExerciseType;
+use App\Models\Instructor;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 
@@ -10,7 +11,11 @@ class ExerciseTypeHasInstructors implements ValidationRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (ExerciseType::wherePivot('instructor_id', $value)) {
+        if (
+            Instructor::whereHas('exerciseTypes', function ($exerciseTypes) use ($value) {
+                $exerciseTypes->where('exercise_type_id', $value);
+            })->exists()
+        ) {
             $fail('Hay instructores asociados al tipo de ejercicio seleccionado.');
         }
     }
--- app/Rules/ExerciseTypeHasSessions.php
+++ app/Rules/ExerciseTypeHasSessions.php
@@ -3,6 +3,7 @@
 namespace App\Rules;
 
 use App\Models\ExerciseType;
+use App\Models\Session;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 
@@ -10,7 +11,7 @@ class ExerciseTypeHasSessions implements ValidationRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (ExerciseType::with('sessions')->where('instructor_id', $value)) {
+        if (Session::where('exercise_type_id', $value)->exists()) {
             $fail('Hay clases asociadas al tipo de ejercicio seleccionado.');
         }
     }
