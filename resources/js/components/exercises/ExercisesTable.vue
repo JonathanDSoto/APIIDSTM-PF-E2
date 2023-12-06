60aeef1993dfdc3f0dcbc9310fa41a7d202383a0
IngenieroAlan <brandon.rod182@gmail.com>
2023-12-05 17:14:20 -0700

    EditExerciseTypeRecord created, ExercisesTable updated

--- resources/js/components/exercises/ExercisesTable.vue
+++ resources/js/components/exercises/ExercisesTable.vue
@@ -1,11 +1,12 @@
 <script setup>
-import DeleteModal from "../DeleteModal.vue";
 import { ref, inject } from 'vue';
+import DeleteModal from "../DeleteModal.vue";
+import EditExerciseTypeRecord from "../edit_record/EditExerciseTypeRecord.vue";
 const exercises = ref(inject('exercises'));
 exercises.value = exercises.value.data;
-console.log(JSON.stringify(exercises.value));
-console.log(exercises.value[0].id);
 const selectedId = ref(exercises.value[0].id);
+const selectedIndex = ref(0);
+console.log(exercises.value[selectedIndex.value]);
 </script>
 <template>
     <div class="table-responsive text-nowrap">
@@ -17,20 +18,29 @@ const selectedId = ref(exercises.value[0].id);
                 </tr>
             </thead>
             <tbody class="table-border-bottom-0">
-                <tr v-for="exercise in exercises">
+                <tr v-for="(exercise, index) in exercises">
                     <td>{{ exercise.name }}</td>
                     <td class="text-end">
-                        <a class="btn btn-success me-1" href="javascript:void(0);">
+                        <button
+                            class="btn btn-success me-1"
+                            href="javascript:void(0);"
+                            @click="selectedIndex = index"
+                            data-bs-toggle="modal"
+                            style="margin: 5px"
+                            data-bs-target="#editExerciseType">
                             <i class="ti ti-pencil "></i>
-                        </a>
-                        <a class="btn btn-danger" href="javascript:void(0);" @click="selectedId = exercise.id"
+                        </button>
+                        <button class="btn btn-danger" href="javascript:void(0);" @click="selectedId = exercise.id"
                             style="margin: 5px" data-bs-toggle="modal" data-bs-target="#deleteUser">
                             <i class="ti ti-trash"></i>
-                        </a>
+                        </button>
                     </td>
                 </tr>
             </tbody>
         </table>
     </div>
+    <EditExerciseTypeRecord
+        :exercise="exercises[selectedIndex]"
+    />
     <DeleteModal function="delete" type="exercise_types" :id="selectedId" />
 </template>
