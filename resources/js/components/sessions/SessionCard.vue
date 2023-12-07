c2028ee74f4494659dd4e86919b0008541420ae2
IngenieroAlan <brandon.rod182@gmail.com>
2023-12-07 01:26:27 -0700

    SessionCard component created

---
+++ resources/js/components/sessions/SessionCard.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps({
+    session: {
+        type: Object,
+        required: true,
+    }
+});
+</script>
+<template>
+    <div class="col-12 col-sm-6 col-lg-3 mb-4">
+        <div class="card position-relative h-100">
+            <div class="card-body text-center">
+                <a :href="route('sessions.show',{id:props.session.id})">
+                    <i class="mb-3 fas fa-dumbbell ti-lg"></i>
+                </a>
+                <h5>{{props.session.name}}</h5>
+                <p style="text-align: justify;">{{props.session.description}}</p>
+                <div class="d-flex flex-column justify-content-between align-items-center mt-3 gap-3">
+                    <small>Capacidad: {{ props.session.max_capacity }} personas</small>
+                    <small>Ejercicio: {{props.session.exercise_type.name}}</small>
+                </div>
+                <div  class="d-flex justify-content-between align-items-end">
+                    <a type="button" class="btn btn-primary mt-3" data-bs-toggle="modal" data-bs-target="#reserveSession"  href="javascript:void(0);">
+                        <i class="ti ti-calendar"></i>
+                    </a>
+                    <a type="button" class="btn btn-success mt-3"  :href="route('sessions.edit',{id:props.session.id})">
+                        <i class="ti ti-pencil"></i>
+                    </a>
+                    <a type="button" class="btn btn-danger mt-3" data-bs-toggle="modal" data-bs-target="#deleteSession" href="javascript:void(0);">
+                        <i class="ti ti-trash"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
