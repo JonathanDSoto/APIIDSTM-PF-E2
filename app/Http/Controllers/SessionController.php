ce9cca04f624df9f9ac9bdb3167b2fef203cfd30
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-12-06 19:29:35 -0700

    Métodos index, show y destroy de sessions implementados

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers;
 
+// use App\Http\Requests\StoreSessionRequest;
+// use App\Http\Requests\UpdateSessionRequest;
+use App\Models\ExerciseType;
+use App\Models\Instructor;
+use App\Models\Session;
+use App\Models\WeekDay;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class SessionController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
-        return view('sessions.index');
+        return view('sessions.index', [
+            'sessions' => Session::with('exerciseType')->get(),
+            'instructors' => Instructor::orderBy('name')->get(),
+            'weekDays' => WeekDay::all(),
+            'exerciseTypes' => ExerciseType::orderBy('name')->get()
+        ]);
     }
 
     public function store(Request $request)
@@ -18,7 +35,33 @@ class SessionController extends Controller
 
     public function show(string $id)
     {
-        return view('sessions.show');
+        $relationships = [
+            'exerciseType',
+            'sessionDays' => function ($sessionDays) {
+                $sessionDays->with([
+                    'instructor',
+                    'weekDay',
+                    'participants',
+                    'attendanceHistory' => function ($attendanceHistory) {
+                        $attendanceHistory->orderBy('attendance_date', 'desc');
+                    }
+                ]);
+            }
+        ];
+
+        try {
+            $session = Session::with($relationships)
+                ->findOrFail($id);
+        } catch (ModelNotFoundException $modelNotFoundException) {
+            return back()
+                ->withErrors([
+                    'internal_error' => 'No se ha podido encontrar la clase solicitada.'
+                ]);
+        }
+
+        return view('sessions.show', [
+            'session' => $session
+        ]);
     }
 
     public function update(Request $request, string $id)
@@ -28,6 +71,18 @@ class SessionController extends Controller
 
     public function destroy(string $id)
     {
-        
+        try {
+            $session = Session::findOrFail($id);
+        } catch (ModelNotFoundException $modelNotFoundException) {
+            return back()
+                ->withErrors([
+                    'internal_error' => 'No se ha podido encontrar la clase solicitada.'
+                ]);
+        }
+
+        $session->delete();
+
+        return back()
+            ->with('success', 'La información de la clase se ha eliminado con éxito.');
     }
 }
