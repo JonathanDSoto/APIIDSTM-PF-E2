36a4ec7c2546a4a12ddd0986518baf83bbd5cb0a
IngenieroAlan <brandon.rod182@gmail.com>
2023-12-08 01:29:17 -0700

    SessionCard design update

--- resources/js/components/sessions/SessionCard.vue
+++ resources/js/components/sessions/SessionCard.vue
@@ -13,23 +13,27 @@ const props = defineProps({
 <template>
     <div class="col-12 col-sm-6 col-lg-3 mb-4">
         <div class="card position-relative h-100">
-            <div class="card-body text-center">
-                <a :href="route('sessions.show',{id:props.session.id})">
-                    <i class="mb-3 fas fa-dumbbell ti-lg"></i>
-                </a>
-                <h5>{{props.session.name}}</h5>
-                <p style="text-align: justify;">{{props.session.description}}</p>
-                <div class="d-flex flex-column justify-content-between align-items-center mt-3 gap-3">
-                    <small>Capacidad: {{ props.session.max_capacity }} personas</small>
-                    <small>Ejercicio: {{props.session.exercise_type.name}}</small>
+            <div class="card-body text-center d-flex flex-column justify-content-between">
+                <div>
+                    <a :href="route('sessions.show',{id:props.session.id})">
+                        <i class="mb-3 fas fa-dumbbell ti-lg"></i>
+                    </a>
+                    <h5>{{props.session.name}}</h5>
+                    <p style="text-align: justify;">{{props.session.description}}</p>
                 </div>
-                <div  class="d-flex justify-content-evenly align-items-end">
-                    <a type="button" class="btn btn-success mt-3"  :href="route('sessions.edit',{id:props.session.id})">
-                        <i class="ti ti-pencil"></i>
-                    </a>
-                    <a type="button" class="btn btn-danger mt-3" @click="this.$emit('set-id',id)" data-bs-toggle="modal" data-bs-target="#deleteUser" href="javascript:void(0);">
-                        <i class="ti ti-trash"></i>
-                    </a>
+                <div>
+                    <div class="d-flex flex-column justify-content-between align-items-center mt-3 gap-3">
+                        <small>Capacidad: {{ props.session.max_capacity }} personas</small>
+                        <small>Ejercicio: {{props.session.exercise_type.name}}</small>
+                    </div>
+                    <div  class="d-flex justify-content-evenly align-items-end">
+                        <a type="button" class="btn btn-success mt-3"  :href="route('sessions.edit',{id:props.session.id})">
+                            <i class="ti ti-pencil"></i>
+                        </a>
+                        <a type="button" class="btn btn-danger mt-3" @click="this.$emit('set-id',id)" data-bs-toggle="modal" data-bs-target="#deleteUser" href="javascript:void(0);">
+                            <i class="ti ti-trash"></i>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
