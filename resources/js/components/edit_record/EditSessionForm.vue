4875c73f4caebbde5a48dd02acb0657b519ccb3f
IngenieroAlan <brandon.rod182@gmail.com>
2023-12-07 22:06:06 -0700

    EditSessionForm working

--- resources/js/components/edit_record/EditSessionForm.vue
+++ resources/js/components/edit_record/EditSessionForm.vue
@@ -1,14 +1,71 @@
 <script setup>
+import { ref, provide, watchEffect } from 'vue';
 import { useForm } from '../hooks/useForm';
-
+import SessionDay from '../sessions/SessionDay.vue';
 const props = defineProps({
     session: {
         type: Object,
         required: true,
     },
+    instructors: {
+        type: Object,
+        required: true,
+    },
+    week_days: {
+        type: Object,
+        required: true,
+    },
+    exercise_types: {
+        type: Object,
+        required: true,
+    },
 });
-const { name, validateEmail } = useForm();
-console.log(props.session);
+provide('instructors', props.instructors);
+provide('week_days', props.week_days);
+const { name, description, max_capacity, validateName, validateMaxCapacity } = useForm();
+const time = ref({
+    start_hour: 0,
+    end_hour: 24,
+});
+const sessionDays = ref([]);
+const sessionDayInput = ref();
+const addSessionDay = () => {
+    sessionDays.value.push({
+        instructor_id: props.instructors[0].id,
+        week_day_id: props.week_days[0].id,
+        start_hour: time.value.start_hour,
+        end_hour: time.value.end_hour,
+    });
+};
+watchEffect(() => {
+    props.session.session_days.forEach((day, index) => {
+        sessionDays.value[index] = {
+            instructor_id: day.instructor_id,
+            week_day_id: day.week_day_id,
+            start_hour: day.start_hour,
+            end_hour: day.end_hour,
+        };
+    });
+});
+watchEffect(() => {
+    name.value = props.session.name;
+    description.value = props.session.description;
+    max_capacity.value = props.session.max_capacity;
+});
+watchEffect(() => {
+    sessionDayInput.value = JSON.stringify(sessionDays.value);
+});
+
+const onSubmit = (event) => {
+    event.preventDefault();
+    let isValid = true;
+    if (!validateName()) { isValid = false; }
+    if (!validateMaxCapacity()) { isValid = false; }
+    if (isValid) {
+        const form = document.querySelector('#editSessionForm');
+        form.submit();
+    }
+};
 </script>
 <template>
     <div class="container">
@@ -20,86 +77,48 @@ console.log(props.session);
                 <h5>Editar una Clase</h5>
             </div>
             <div class="card-body">
-                <form @submit.prevent="true" :href="route('sessions.update', { id: props.session.id })">
+                <form method="POST" @submit.prevent="onSubmit" id="editSessionForm"
+                    :action="route('sessions.update', { id: props.session.id })">
+                    <FormAuth method="PUT" />
                     <div class="form-group mt-2">
                         <label for="nombreClase">Nombre de la Clase:</label>
-                        <input type="text" class="form-control" id="nombreClase"
+                        <input type="text" v-model="name" name="name" class="form-control" id="nombreClase"
                             placeholder="Ingrese el nombre de la clase">
                     </div>
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group mt-2">
-                                <label for="tipoEjercicio">Tipo de Ejercicio:</label>
-                                <input type="text" class="form-control mt-1 text-muted"
-                                    :value="props.session.exercise_type.name" readonly />
+                                <label for="exercises">Tipo de Ejercicio:</label>
+                                <input type="text" class="form-control mt-1" id="exercises" :value="props.session.exercise_type.name" readonly>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group mt-2">
                                 <label for="capacidadMaxima">Capacidad Máxima:</label>
-                                <input type="number" class="form-control mt-1" id="capacidadMaxima" min="5" max="50"
-                                    placeholder="Ingrese la capacidad máxima">
+                                <input type="number" v-model="max_capacity" name="max_capacity" class="form-control mt-1"
+                                    id="capacidadMaxima" placeholder="Ingrese la capacidad máxima">
                             </div>
                         </div>
                     </div>
                     <div class="form-group mt-2">
                         <label for="descripcion">Descripción:</label>
-                        <textarea class="form-control" id="descripcion" rows="1"
-                            placeholder="Ingrese una descripción"></textarea>
+                        <textarea class="form-control" minlength="10" v-model="description" id="descripcion"
+                            name="description" rows="3" placeholder="Ingrese una descripción"></textarea>
                     </div>
-                    <hr class="my-4">
+                    <hr class="mt-4">
                     <h5 class="text-center">Horarios</h5>
-
-                    <div class="row">
-                        <div class="col-md-5">
-                            <div class="form-group">
-                                <label for="instructor">Seleccionar un Instructor:</label>
-                                <select class="form-control" id="instructor">
-                                    <option value="instructor1">Aaron Meza</option>
-                                    <option value="instructor2">Dario Garcia</option>
-                                    <option value="instructor3">Jose Cervantes</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label for="dia">Seleccionar un Día:</label>
-                                <select class="form-control" id="dia">
-                                    <option value="lunes">Lunes</option>
-                                    <option value="martes">Martes</option>
-                                    <option value="miercoles">Miércoles</option>
-                                    <option value="jueves">Jueves</option>
-                                    <option value="viernes">Viernes</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label for="horaInicio">Hora de Inicio:</label>
-                                <input type="text" class="form-control" id="horaInicio">
-                            </div>
-                        </div>
-                        <div class="col-md-2">
-                            <div class="form-group">
-                                <label for="horaFinal">Hora Final:</label>
-                                <input type="text" class="form-control" id="horaFinal">
-                            </div>
-                        </div>
-                        <div class="col-md-1">
-                            <div class="form-group mt-4">
-                                <button type="submit" class="btn btn-danger ti ti-trash"></button>
-                            </div>
-                        </div>
-                    </div>
+                    <SessionDay v-for="(sessionDay, index) in sessionDays" :init_values="sessionDay"
+                        :sessionDays="sessionDays" :index="index" />
+                    <input type="hidden" name="session_days" v-model="sessionDayInput" />
                     <div class="row">
                         <div class="col-12 d-flex justify-content-center align-items-center">
-                            <button type="submit" class="btn btn-primary ti ti-plus mt-2 bi-lg"></button>
+                            <button type="button" @click="addSessionDay()"
+                                class="btn btn-primary ti ti-plus mt-2 bi-lg"></button>
                         </div>
                     </div>
                     <div class="col-12 text-center mt-3">
                         <button type="submit" class="btn btn-primary me-sm-3 me-1">Guardar</button>
-                        <button type="reset" class="btn btn-label-secondary" data-bs-dismiss="modal"
-                            aria-label="Close">Cancelar</button>
+                        <a :href="route('sessions')" class="btn btn-label-secondary">Cancelar</a>
                     </div>
                 </form>
             </div>
--- resources/views/sessions/edit.blade.php
+++ resources/views/sessions/edit.blade.php
@@ -1,4 +1,20 @@
 @extends('layouts.app')
 @section('contenido')
-<edit-session-form :session="{{$session}}"></edit-session-form>
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+@if (session('success'))
+<div class="alert alert-success alert-dismissible" role="alert">
+    {{ session('success') }}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+    </button>
+</div>
+@endif
+<edit-session-form :session="{{$session}}" :instructors="{{json_encode($instructors)}}" :exercise_types="{{$exerciseTypes}}" :week_days="{{$weekDays}}"></edit-session-form>
 @endsection
